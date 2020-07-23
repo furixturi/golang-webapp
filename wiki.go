b11805c0eb8e95ecdcf733df2cc675f6ce7f752e
furixturi <furixturi@gmail.com>
2020-07-23 23:51:14 +0900

    initial commit

---
+++ wiki.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+)
+
+/********************
+ *  Data Structure  *
+*********************/
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+/****************
+ *  Util Funcs  *
+*****************/
+var templates = template.Must(template.ParseFiles("templates/edit.html", "templates/view.html"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	// Don't do this, which loads the template file again and again
+	// whenever the page is visited
+	// t, err := template.ParseFiles("templates/" + tmpl + ".html")
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+	// err = t.Execute(w, p)
+
+	// Cache the templates first and use this
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (p *Page) save() error {
+	filename := "data/" + p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := "data/" + title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+var validPathRegex = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPathRegex.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			// http.NotFound(w, r)
+			http.Error(w, "Invalid Path. Only alphanumeric allowed.", http.StatusBadRequest)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+
+/********************
+ *	Route Handlers  *
+*********************/
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound) // 302
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+/****************
+*  Entry Point  *
+*****************/
+func main() {
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+	log.Fatal(http.ListenAndServe(":8888", nil))
+}
